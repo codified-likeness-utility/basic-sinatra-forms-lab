796ebaff92c791afe7badc0a1e28906415141eb4
Ian Davies <mr.daviesian@gmail.com>
2021-03-16 17:08:54 -0500

    commit tree fb404e300c8f41f08e28b00e0c74bcfbb5724b01

--- app.rb
+++ app.rb
@@ -2,5 +2,14 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do 
+        new_team = params
+        erb :newteam
+
+    end
+
+    post '/team' do
+        erb :team
+    end
 
 end
